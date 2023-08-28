56222823a1bdbfaeac065b3659dc9d126569dd87
Moshe Rubin <mosherubin137@gmail.com>
2023-08-28 18:54:18 -0500

    Initial solution `2222222222`

---
+++ 2222222222/2222222222.py
@@ -0,0 +1,33 @@
+import functools
+import itertools
+import math
+
+from tqdm import tqdm as og_tqdm
+
+string = "2" * 10  # 2222222222
+string += "**"  # 2222222222**
+
+already_tried = []
+max_result = 0
+
+tqdm = functools.partial(
+    og_tqdm,
+    dynamic_ncols=True,
+)
+
+for trial in tqdm(itertools.permutations(string), total=math.factorial(len(string))):
+    if trial in already_tried:
+        continue
+    trial_str = "".join(trial)
+    if "**" in trial_str:
+        continue
+    already_tried.append(trial)
+    try:
+        result = eval(trial_str)
+        if result > max_result:
+            max_result = result
+            max_expr = trial_str
+    except SyntaxError:
+        continue
+
+print(f"{max_expr} = {max_result:,}")

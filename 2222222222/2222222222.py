5d328a5d60f9edfce687bddcb2dc1ad6e2d761ba
Moshe Rubin <mosherubin137@gmail.com>
2023-08-29 16:02:15 -0500

    Move code to function

--- 2222222222/2222222222.py
+++ 2222222222/2222222222.py
@@ -4,27 +4,34 @@ import math
 
 from tqdm import tqdm as og_tqdm
 
-string = "2" * 10  # 2222222222
-string += "**"  # 2222222222**
-
-already_tried = []
-max_result = 0
-
 tqdm = functools.partial(
     og_tqdm,
     dynamic_ncols=True,
 )
 
-for trial in tqdm(itertools.permutations(string), total=math.factorial(len(string))):
-    if trial in already_tried:
-        continue
-    trial_str = "".join(trial)
-    if "**" in trial_str or trial_str.startswith("*") or trial_str.endswith("*"):
-        continue
-    already_tried.append(trial)
-    result = eval(trial_str)
-    if result > max_result:
-        max_result = result
-        max_expr = trial_str
+string = "2" * 10  # 2222222222
 
-print(f"{max_expr} = {max_result:,}")
+
+def solution_O_n_factorial() -> None:
+    string_star = string + "**"  # 2222222222**
+    already_tried = []
+    max_result = 0
+
+    for trial in tqdm(
+        itertools.permutations(string_star), total=math.factorial(len(string_star))
+    ):
+        if trial in already_tried:
+            continue
+        trial_str = "".join(trial)
+        if "**" in trial_str or trial_str.startswith("*") or trial_str.endswith("*"):
+            continue
+        already_tried.append(trial)
+        result = eval(trial_str)
+        if result > max_result:
+            max_result = result
+            max_expr = trial_str
+
+    print(f"{max_expr} = {max_result:,}")
+
+
+solution_O_n_factorial()

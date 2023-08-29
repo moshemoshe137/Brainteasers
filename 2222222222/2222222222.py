058b80663f21cd6c7ed1c7fee20ccfe2aecf304f
Moshe Rubin <mosherubin137@gmail.com>
2023-08-29 16:24:28 -0500

    Create linear solution

--- 2222222222/2222222222.py
+++ 2222222222/2222222222.py
@@ -34,4 +34,28 @@ def solution_O_n_factorial() -> None:
     print(f"{max_expr} = {max_result:,}")
 
 
-solution_O_n_factorial()
+# solution_O_n_factorial()
+
+
+def solution_O_n() -> None:
+    max_result = 0
+
+    for first_times_pos in range(1, len(string)):
+        for second_times_pos in range(first_times_pos + 1, len(string) - 1):
+            left = first_times_pos
+            middle = second_times_pos - left
+            right = len(string) - second_times_pos
+            # middle = len(string) - left - right
+
+            left2 = int("2" * left)
+            middle2 = int("2" * middle)
+            right2 = int("2" * right)
+
+            result = left2 * middle2 * right2
+            if result > max_result:
+                max_result = result
+                max_expr = f"{left2:,} * {middle2:,} * {right2:,} = {max_result:,}"
+    print(max_expr)
+
+
+solution_O_n()

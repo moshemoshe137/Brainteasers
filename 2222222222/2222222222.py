1ce02717561f27b3266020032bd1b8af98509017
Moshe Rubin <mosherubin137@gmail.com>
2023-08-29 16:28:03 -0500

    Remove inefficient solution

--- 2222222222/2222222222.py
+++ 2222222222/2222222222.py
@@ -1,43 +1,7 @@
-import functools
-import itertools
-import math
-
-from tqdm import tqdm as og_tqdm
-
-tqdm = functools.partial(
-    og_tqdm,
-    dynamic_ncols=True,
-)
-
 string = "2" * 10  # 2222222222
 
 
-def solution_O_n_factorial() -> None:
-    string_star = string + "**"  # 2222222222**
-    already_tried = []
-    max_result = 0
-
-    for trial in tqdm(
-        itertools.permutations(string_star), total=math.factorial(len(string_star))
-    ):
-        if trial in already_tried:
-            continue
-        trial_str = "".join(trial)
-        if "**" in trial_str or trial_str.startswith("*") or trial_str.endswith("*"):
-            continue
-        already_tried.append(trial)
-        result = eval(trial_str)
-        if result > max_result:
-            max_result = result
-            max_expr = trial_str
-
-    print(f"{max_expr} = {max_result:,}")
-
-
-# solution_O_n_factorial()
-
-
-def solution_O_n() -> None:
+def solution() -> None:
     max_result = 0
 
     for first_times_pos in range(1, len(string)):
@@ -58,4 +22,4 @@ def solution_O_n() -> None:
     print(max_expr)
 
 
-solution_O_n()
+solution()
